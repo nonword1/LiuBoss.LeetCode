e2c4f30a2303699134755d7ef3eb0eddc72b9749
liuboss1992 <liuboss1992@outlook.com>
2020-04-18 13:32:47 +0800

    del

--- Code/349.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        itt = []
-        for i in nums1:
-            if (i in nums2) and (i not in itt):
-                itt.append(i)
-        return itt
-
--- code/136.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        s = 0
-        for i in(nums):
-            s ^= i
-        return s
